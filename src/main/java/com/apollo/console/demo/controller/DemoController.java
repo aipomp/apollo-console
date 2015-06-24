a0480c4cfc264eb03a764f52ea04d4e40050d452
zhouletian <letianok@qq.com>
2015-06-24 10:32:39 +0800

    #修改人：周乐天 #修改原因：增加异常处理测试

--- src/main/java/com/apollo/console/demo/controller/DemoController.java
+++ src/main/java/com/apollo/console/demo/controller/DemoController.java
@@ -1,5 +1,7 @@
 package com.apollo.console.demo.controller;
 
+import java.sql.SQLException;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -7,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.apollo.console.common.controller.BaseController;
 import com.apollo.console.demo.bean.CompInst;
 import com.apollo.console.demo.service.CompInstService;
 
@@ -17,15 +20,20 @@ import com.apollo.console.demo.service.CompInstService;
  */
 @Controller
 @RequestMapping("/demo/test")
-public class DemoController {
+public class DemoController  extends BaseController{
 	@Autowired
 	private CompInstService compInstService;
 	
 	
 	@RequestMapping(value="/getCompDesc")
 	public void startStopActivity(HttpServletRequest request, HttpServletResponse response) throws Exception{
-		long compInstId = 1000096;
-		CompInst compInst = compInstService.selectByPrimaryKey(compInstId);
-		response.getWriter().print(compInst.getCompInstDesc());
+		//异常处理test	
+		throw new SQLException();
+//		long compInstId = Long.parseLong(request.getParameter("compInstId"));
+//		String name = request.getParameter("name");
+//		
+//		log.info("中文的参数：" +  name);
+//		CompInst compInst = compInstService.selectByPrimaryKey(compInstId);
+//		response.getWriter().print(compInst.getCompInstDesc());
 	}
 }
