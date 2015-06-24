71d42e1fabbbf9450cb8aabb884f86c8ec480d91
zhouletian <letianok@qq.com>
2015-06-24 10:15:17 +0800

    #修改人：周乐天 #修改原因：增加分页及分页的demo

--- src/main/java/com/apollo/console/demo/service/impl/CompInstServiceImpl.java
+++ src/main/java/com/apollo/console/demo/service/impl/CompInstServiceImpl.java
@@ -1,12 +1,17 @@
 package com.apollo.console.demo.service.impl;
 
+import java.util.List;
+
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
 
+import com.alibaba.fastjson.JSON;
 import com.apollo.console.baseapi.demo.CompInstMapper;
 import com.apollo.console.demo.bean.CompInst;
 import com.apollo.console.demo.service.CompInstService;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 
 /**
   * @author zhoult
@@ -26,6 +31,16 @@ public class CompInstServiceImpl implements CompInstService {
 	 * 
 	 */
 	public CompInst selectByPrimaryKey(Long compInstId) {
+		//测试分页
+		PageHelper.startPage(1, 10);
+		List<CompInst> root = compInstMapper.selectAll();
+		//用PageInfo对结果进行包装
+		PageInfo page = new PageInfo(root);
+		//测试PageInfo全部属性
+		//PageInfo包含了非常全面的分页属性
+		System.out.println(page.getPageNum());
+		String jsonString = JSON.toJSONString(page);
+		System.out.println(jsonString);
 		// TODO Auto-generated method stub
 		return compInstMapper.selectByPrimaryKey(compInstId);
 	}
